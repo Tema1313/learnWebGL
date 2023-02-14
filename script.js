c403db92091feb4c00ba5abc2ca1cedb51a333bf
Tema1313 <88036915+Tema1313@users.noreply.github.com>
2023-02-14 16:48:04 +0700

    Убрал пиксельность, добавил два квадрата

--- script.js
+++ script.js
@@ -13,33 +13,67 @@ function main() {
 
   const scene = new THREE.Scene();
 
+  {
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
+
   const boxWidth = 1;
   const boxHeight = 1;
   const boxDepth = 1;
   const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-  const material = new THREE.MeshPhongMaterial({color: 0x44aa88});  // Есть световое
+  function makeInstance(geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({color});
 
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-  const color = 0xFFFFFF;
-  const intensity = 1;
-  const light = new THREE.DirectionalLight(color, intensity);
-  light.position.set(-1, 2, 4);
-  scene.add(light);
+    cube.position.x = x;
+
+    return cube;
+  }
+
+  const cubes = [
+    makeInstance(geometry, 0x44aa88,  0),
+    makeInstance(geometry, 0x8844aa, -2),
+    makeInstance(geometry, 0xaa8844,  2),
+  ];
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false); //устанавливаем внутренний размер холста(буфер рисования холста)
+    }
+    return needResize;
+  }
 
   function render(time) {
-    time *= 0.001;  // Преобразовать время до секунды
-
-    cube.rotation.x = time;
-    cube.rotation.y = time;
+    time *= 0.001;  //конвертировать время в секунды
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * .1;
+      const rot = time * speed;
+      cube.rotation.x = rot;
+      cube.rotation.y = rot;
+    });
 
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
   }
   requestAnimationFrame(render);
+
 }
 
 main();
